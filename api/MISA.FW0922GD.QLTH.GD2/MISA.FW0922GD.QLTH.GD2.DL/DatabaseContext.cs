8418767291c821fa38e983ec84620979a13967ce
Nguyen Duy Khai <khai12345678t@gmail.com>
2022-12-21 15:30:25 +0700

    chore: Định nghĩa, khai báo ConnectionString và dựng các file, thư mục dùng chung.

---
+++ api/MISA.FW0922GD.QLTH.GD2/MISA.FW0922GD.QLTH.GD2.DL/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.FW0922GD.QLTH.GD2.DL
+{
+    /// <summary>
+    /// Định nghĩa các thông tin cần thiết để kết nối và thao tác với Database
+    /// </summary>
+    /// Author: KhaiND (21/12/2022)
+    public class DatabaseContext
+    {
+        #region Field
+
+        /// <summary>
+        /// ConnectionString để kết nối đến Database
+        /// </summary>
+        public static string ConnectionString = "";
+
+        #endregion
+    }
+}
